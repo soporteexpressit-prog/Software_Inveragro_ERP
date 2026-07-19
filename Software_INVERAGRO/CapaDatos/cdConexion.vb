d2b7397ec6a5840798735fec0cc303b968474ff6
IsaiasFloresJH <127262340+IsaiasFloresJH@users.noreply.github.com>
2026-07-19 00:30:19 -0500

    mejora del reporte para ver distribuciones

--- Software_INVERAGRO/CapaDatos/cdConexion.vb
+++ Software_INVERAGRO/CapaDatos/cdConexion.vb
@@ -2,8 +2,8 @@
 
 Public Class cdConexion
 
-    Private Shared ReadOnly conexionProduccion As String = "Data Source=161.132.55.56,1433;Initial Catalog=BDINVERAGRO;User ID=SuperA;Password=!!Inveragro26"
-    Private Shared ReadOnly conexionPruebas As String = "Data Source=161.132.55.56,1433;Initial Catalog=BDINVERAGRO_TEST;User ID=SuperA;Password=!!Inveragro26"
+    Private Shared ReadOnly conexionProduccion As String = "Data Source=localhost,1433;Initial Catalog=BDINVERAGRO;User ID=SuperA;Password=!!Inveragro26"
+    Private Shared ReadOnly conexionPruebas As String = "Data Source=localhost,1433;Initial Catalog=BDINVERAGRO_TEST;User ID=SuperA;Password=!!Inveragro26"
     Public Shared EsProduccion As Boolean = False
 
     Public con As SqlConnection
